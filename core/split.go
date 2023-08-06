809b45fe5adb5a75f8f7b69c4d8b41c155a9715d
no-yan <63000297+no-yan@users.noreply.github.com>
2023-08-06 23:28:11 +0900

    ナイーブな実装

--- core/split.go
+++ core/split.go
@@ -1,5 +1,17 @@
 package core
 
-func Split(s string) string {
-	return s
+func Split(s string) []string {
+	chunkSize := 1000
+	var result []string
+
+	for i := 0; i < (len(s)+chunkSize-1)/chunkSize; i++ {
+		start := i * chunkSize
+		end := start + chunkSize
+		if end > len(s) {
+			end = len(s)
+		}
+		result = append(result, s[start:end])
+	}
+
+	return result
 }
--- core/split_test.go
+++ core/split_test.go
@@ -2,16 +2,31 @@ package core
 
 import "testing"
 
+var empty []string
+
+func Equal(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func Test(t *testing.T) {
 	cases := []struct {
-		in, want string
+		in   string
+		want []string
 	}{
-		{"", ""},
-		{"hello", "hello"},
+		{"", []string{}},
+		{"hello", []string{"hello"}},
 	}
 	for _, c := range cases {
 		got := Split(c.in)
-		if got != c.want {
+		if !Equal(got, c.want) {
 			t.Errorf("Split(%q) == %q, want %q", c.in, got, c.want)
 		}
 	}
