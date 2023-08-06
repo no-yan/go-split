7a7c9f419553357a806cd73bc52d2cb229af9eb2
no-yan <63000297+no-yan@users.noreply.github.com>
2023-08-06 21:20:45 +0900

    init

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func Test(t *testing.T) {
+	// cases := []struct {
+	// 	in, want string
+	// }{
+	// 	{"", ""},
+	// 	// {"hello", "hello"},
+	// }
+	// for _, c := range cases {
+
+	// }
+	main()
+	// Output: hello
+}
