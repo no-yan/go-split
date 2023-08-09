2e2665d8d3e493c93dd4faeff9ce1c9f4ff161c3
no-yan <63000297+no-yan@users.noreply.github.com>
2023-08-10 03:43:17 +0900

    複数ファイルの書き込みに対応

--- core/split.go
+++ core/split.go
@@ -13,30 +13,37 @@ func Split(r io.Reader, w NewWriterFunc) {
 	lineLim := 5000
 
 	scanner := bufio.NewScanner(r)
-	bw := bufio.NewWriter(w())
+
 	// FIXME: bufioは行が65536文字を超えるとエラーが発生する。
 	// https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go
-	next := scanner.Scan()
-	defer bw.Flush()
 
-	for i := 0; i < lineLim; i++ {
-		e := scanner.Err()
-		if next == false {
-			if e != nil {
-				fmt.Fprintln(os.Stderr, "reading standard input:", e)
+	for next := scanner.Scan(); next; {
+		bw := bufio.NewWriter(w())
+		defer bw.Flush()
+
+		for i := 0; i < lineLim; i++ {
+			if next == false {
+				if e := scanner.Err(); e != nil {
+					fmt.Fprintln(os.Stderr, "reading standard input:", e)
+				}
+
+				// EOF
+				txt := scanner.Text()
+				if len(txt) > 0 {
+					// In POSIX, EOF should be "\n".
+					// If not, add it.
+					fmt.Println(bw, txt)
+				}
+				break
 			}
 
-			// EOF
-			txt := scanner.Text()
-			fmt.Fprint(bw, txt)
-			if len(txt) != 0 {
-				fmt.Fprint(bw, "/n")
-			}
-			break
+			fmt.Fprintln(bw, scanner.Text())
+			next = scanner.Scan()
 		}
 
-		fmt.Fprintln(bw, scanner.Text())
-		next = scanner.Scan()
+		if next == false && scanner.Err() == nil {
+			break
+		}
 	}
 
 }
